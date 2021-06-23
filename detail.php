85d0cfc4aaf9c295c71a2aa8b214d92e4e2de988
Mauro Rocca <mauroroccaf@gmail.com>
2021-06-23 01:19:04 -0300

    para subir a heroku

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@ error_reporting(E_ERROR & E_PARSE);
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
-$base_url = "http://localhost:8081/mp-ecommerce-php/";
+$base_url = "https://mp-ecommerce-php-mrocca.herokuapp.com/";
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
