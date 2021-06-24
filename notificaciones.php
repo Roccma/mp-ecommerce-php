fbecfbd8d3ab0663f981ab760d2d8f932221bda5
Mauro Rocca <mauroroccaf@gmail.com>
2021-06-23 22:49:06 -0300

    para subir a heroku

--- notificaciones.php
+++ notificaciones.php
@@ -7,12 +7,13 @@
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
     $fp = fopen('logs.txt', 'a');
+    fwrite( $fp , var_export($_REQUEST, true));
+    fclose( $fp );
     
     switch($_POST["type"]) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            fwrite( $fp , var_export($payment, true));
-            fclose( $fp );
+            
             break;
         case "plan":
             $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
